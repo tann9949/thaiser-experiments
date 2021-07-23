99a758dadfe14fa21d3d5298a084d000fbd091cf
tann9949 <tan_epacsp@hotmail.com>
2021-07-23 09:06:48 +0000

    remove data trimming

--- experiment/data/thaiser.py
+++ experiment/data/thaiser.py
@@ -75,7 +75,7 @@ class ThaiSERLoader(BaseDataLoader):
         label = label[label["agreement"] > self.agreement];
         label = label[label["mic"] == self.train_mic];
         
-        train: pd.DataFrame = label[label["studio_id"].map(lambda x: int(x[1:]) in self.train_studios)].iloc[:50];
+        train: pd.DataFrame = label[label["studio_id"].map(lambda x: int(x[1:]) in self.train_studios)];
 
         if self.include_zoom:
             train = pd.concat([train, zoom], axis=0).reset_index(drop=True);
@@ -111,7 +111,7 @@ class ThaiSERLoader(BaseDataLoader):
         label = label[label["agreement"] > self.agreement];
         label = label[label["mic"] == self.train_mic];
         
-        val: pd.DataFrame = label[label["studio_id"].map(lambda x: int(x[1:]) in self.val_studios)].iloc[:50];
+        val: pd.DataFrame = label[label["studio_id"].map(lambda x: int(x[1:]) in self.val_studios)];
         scores: np.ndarray = val[self.score_cols].values.astype(float);
         paths: np.ndarray = val["path"].values;
         
@@ -143,7 +143,7 @@ class ThaiSERLoader(BaseDataLoader):
         label = label[label["agreement"] > self.agreement];
         label = label[label["mic"] == self.test_mic];
         
-        test: pd.DataFrame = label[label["studio_id"].map(lambda x: int(x[1:]) in self.test_studios)].iloc[:50];
+        test: pd.DataFrame = label[label["studio_id"].map(lambda x: int(x[1:]) in self.test_studios)];
         scores: np.ndarray = test[self.score_cols].values.astype(float);
         paths: np.ndarray = test["path"].values;
         
