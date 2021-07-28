d436e9e586e68f47164af2f9e8a8a7aabab7a803
Chompakorn CChaichot <tan_epacsp@hotmail.com>
2021-07-29 00:21:33 +0700

    refactor train script

--- experiment/utils.py
+++ experiment/utils.py
@@ -1,8 +1,111 @@
 import os
+from typing import Any, Dict
 
+import yaml
 from line_notify import LineNotify
 
 ACCESS_TOKEN = open(f"{os.path.dirname(__file__)}/../LINE_TOKEN.txt").readlines()[0].strip();
+
+
+def load_yaml(yaml_path: str) -> Dict[str, Any]:
+    """
+    Read .yaml file and stored in dictionary
+
+    Argument
+    --------
+    yaml_path: str
+        Path to yaml to read
+    
+    Return
+    ------
+    data: Dict[str, Any]
+        Dictionary containing yaml data
+    """
+    with open(yaml_path, 'r') as stream:
+        return yaml.safe_load(stream);
+
+
+def read_config(config_path: str) -> Dict[str, Any]:
+    """
+    Read config file to get hyperparameters
+
+    Featurizer
+        
+
+    Argument
+    --------
+    config_path: str
+        Path to config file to read in .yaml format
+    
+    Return
+    ------
+    hparams: Dict[str, Any]
+    """
+    config: Dict[str, Any] = load_yaml(config_path);  # load config file
+
+    #### unpack ####
+
+    # data
+    data: Dict[str, Any] = config.get("data", {});
+    test_mics: str = data.get("test_mic", ["con"]);
+    test_zoom: bool = data.get("test_zoom", True);
+    dataloader_param: Dict[str, Any] = data.pop("dataloader") if "dataloader" in data.keys() else {};
+    include_zoom: bool = dataloader_param.get("include_zoom", False);
+
+    # feautre
+    feature: Dict[str, Any] = config.get("featurizer", {});
+    
+    # packer
+    packer: Dict[str, Any] = config.get("packer", {});
+
+    # model
+    feature_type: str = feature.get("feat_type", "fbank");
+    feature_param: Dict[str, Any] = feature.get("feature_param", {});
+    if feature_type == "fbank":
+        in_channel: int = feature_param.get("num_mel_bins", 40);
+    elif feature_type == "spectrogram":
+        # TODO:
+        pass
+    elif feature_type == "mfcc":
+        # TODO:
+        pass
+    max_len: int = packer.get("max_len", 3);
+    frame_shift: int = feature_param.get("frame_shift", 10);
+    include_fru: bool = dataloader_param.get("include_fru", False);
+
+    n_class: int = 5 if include_fru else 4;
+    sequence_length: int = int(max_len * (1000 / frame_shift))
+
+    model_hparam: Dict[str, Any] = {
+        "in_channel": in_channel,
+        "sequence_length": sequence_length,
+        "n_classes": n_class,
+        **config.get("model", {})
+    }
+
+    # train
+    train: Dict[str, Any] = config.get("train", {});
+    trainer_param: Dict[str, Any] = train.pop("trainer_param") if "trainer_param" in train.keys() else {};
+    batch_size: int = train.get("batch_size", 64);
+    n_iteration: int = train.get("n_iteration", 25);
+    exp_path: str = train.get("exp_path", "log/exp");
+
+    if test_zoom and include_zoom:
+        raise ValueError(f"Cannot parse `test_zoom` and `include_zoom` at the same time")
+
+    return {
+        "featurizer": feature,
+        "packer": packer,
+        "dataloader": dataloader_param,
+        "model_hparams": model_hparam,
+        "trainer_params": trainer_param,
+        "frame_size": frame_shift/1000,
+        "test_mics": test_mics,
+        "test_zoom": test_zoom,
+        "batch_size": batch_size,
+        "n_iteration": n_iteration,
+        "exp_path": exp_path
+    }
 
 
 def notify_line(template: str) -> None:
--- run_thaiser.py
+++ run_thaiser.py
@@ -1,83 +1,53 @@
-from typing import Dict, Optional, Tuple, Any, List
+from argparse import ArgumentParser, Namespace
 import json
-import warnings
 import logging
+import warnings
+from typing import Any, Dict, List, Optional, Tuple
 
 import pytorch_lightning as pl
+from torch.utils import data
 from torch.utils.data import DataLoader
 
-from experiment.experiment_wrapper import ExperimentWrapper
 from experiment.data.feature.feature_packer import FeaturePacker
 from experiment.data.feature.featurizer import Featurizer
 from experiment.data.thaiser import ThaiSERLoader
+from experiment.experiment_wrapper import ExperimentWrapper
 from experiment.model.cnnlstm import CNNLSTM
+from experiment.utils import read_config
 
 warnings.filterwarnings("ignore")
 pl.utilities.distributed.log.setLevel(logging.ERROR)
 
 
-#### HYPER PARAMETERS ####
-label_path: str = "dataset/wav/THAISER/labels.csv";
+def run_parser() ->  Namespace:
+    """
+    Run argument parser
 
-agreement: float = 0.71;
-smoothing_param: float = 0.;
-use_soft_target: bool = False;
-include_fru: bool = False;
-include_zoom: bool = False;
-train_mic: str = "con";
-test_mic: Optional[str] = None;
-    
-num_mel_bins: int = 40;
-max_len: int = 3
-frame_length: int = 50;
-frame_shift: int = 10;
-vtlp_range: Optional[Tuple[float, float]] = (0.9, 1.1);
-n_class: int = 5 if include_fru else 4;
-pad_mode: str = "dup";
-stats_path: Optional[str] = None; # "stats.pckl";
-
-hparams: Dict[str, Any] = {
-    "in_channel": num_mel_bins,
-    "sequence_length": int(max_len * (1000 / frame_shift)),
-    "n_channels": [64, 64, 128, 128],
-    "kernel_size": [5, 3, 3, 3],
-    "pool_size": [4, 2, 2, 2],
-    "lstm_unit": 128,
-    "n_classes": n_class
-}
-    
-batch_size: int = 64;
-max_epoch: int = 2;
-# max_epoch: int = 40;
-n_iteration: int = 2;
-# n_iteration: int = 25;
-gpus: Optional[List[int]] = None;
-# gpus: Optional[List[int]] = [0]; 
-exp_path: str = "log/cnnlstm_exp";
+    Return
+    ------
+    args: Namespace
+        Arguments of the program
+    """
+    parser = ArgumentParser();
+    parser.add_argument("--config-path", required=True, type=str, help="Path to training config file");
+    return parser.parse_args();
     
 
-def main():
+def main(args: Namespace) -> None:
+    # unpack config
+    config: Dict[str, Any] = read_config(args.config_path);
+    frame_size: float = config["frame_size"];
+    test_mics: List[str] = config["test_mics"];
+    test_zoom: bool = config["test_zoom"];
+    batch_size: int = config["batch_size"];
+    n_iteration: int = config["n_iteration"];
+    exp_path: str = config["exp_path"];
+
     #### Featurizer, Packer ####
-    featurizer: Featurizer = Featurizer(
-        feature_type="fbank",
-        feature_param={
-            "frame_length": frame_length,
-            "frame_shift": frame_shift,
-            "num_mel_bins": num_mel_bins,
-            "low_freq": 0,
-            "high_freq": 8000,
-            "sample_frequency": 16000
-        },
-        vtlp_range=vtlp_range
-    );
-        
-    packer: FeaturePacker = FeaturePacker(
-        pad_mode=pad_mode,
-        max_len=max_len,
-        stats_path=stats_path
-    );
+    featurizer: Featurizer = Featurizer(**config["featurizer"]);
+    packer: FeaturePacker = FeaturePacker(**config["packer"]);
 
-
+    # init result statistics
     fold_stats: Dict[str, Any] = {};
 
     # iterate over fold ( 8 folds for THAI SER)
@@ -87,36 +57,51 @@ def main():
         print(f"Running Fold {fold}");
         print("*"*20);
 
-        dataloader: ThaiSERLoader = ThaiSERLoader(
-            fold=fold,
-            use_soft_target=use_soft_target,
-            train_mic=train_mic,
-            test_mic=test_mic,
-            smoothing_param=smoothing_param,
-            label_path=label_path,
-            agreement=agreement,
-            featurizer=featurizer,
-            packer=packer
-        );
+        dataloader: ThaiSERLoader = ThaiSERLoader(**config["dataloader"]);
 
+        # prepare train, val data loaders
         dataloader.setup();
-        train_dataloader, val_dataloader, test_dataloader = dataloader.prepare(frame_size=frame_shift/1000, batch_size=batch_size);
-        if not include_zoom:
-            zoom_dataloader: DataLoader = dataloader.prepare_zoom(frame_size=frame_shift/1000);
-            test_dataloader: Dict[str, DataLoader] = {"TEST": test_dataloader, "ZOOM": zoom_dataloader};
+        train_dataloader, val_dataloader = dataloader.prepare(frame_size=frame_size, batch_size=batch_size);
+
+        # prepare test loaders
+        test_loaders: Dict[str, DataLoader] = {};
+        for mic in test_mics:
+            test_loaders = { 
+                **test_loaders,
+                f"TEST-{mic}": dataloader.prepare_test(frame_size=frame_size, mic_type=mic),
+            }
+        if test_zoom:
+            test_loaders = { 
+                **test_loaders,
+                "ZOOM": dataloader.prepare_zoom(frame_size=frame_size)
+            }
+        if dataloader.cross_corpus is not None:
+            for dataset_name, _ in dataloader.cross_corpus.items():
+                if dataset_name.lower().strip() == "iemocap":
+                    test_loaders = {
+                        **test_loaders,
+                        "IEMOCAP_IMPRO": dataloader.prepare_iemocap(frame_size=frame_size, turn_type="impro"),
+                        "IEMOCAP_SCRIPT": dataloader.prepare_iemocap(frame_size=frame_size, turn_type="script"),
+                        "IEMOCAP": dataloader.prepare_iemocap(frame_size=frame_size),
+                    }
+                elif dataset_name.lower().strip() == "emodb":
+                    test_loaders = {
+                        **test_loaders,
+                        "EMODB": dataloader.prepare_emodb(frame_size=frame_size),
+                    }
+                elif dataset_name.lower().strip() == "emovo":
+                    test_loaders = {
+                        **test_loaders,
+                        "EMOVO": dataloader.prepare_emovo(frame_size=frame_size),
+                    }
 
         wrapper: ExperimentWrapper = ExperimentWrapper(
             ModelClass=CNNLSTM,
-            hparams=hparams,
-            trainer_params={
-                "max_epochs": max_epoch, 
-                "progress_bar_refresh_rate": 0,
-                "weights_summary": None,
-                "gpus": gpus
-            },
+            hparams=config["model_hparams"],
+            trainer_params=config["trainer_params"],
             train_dataloader=train_dataloader,
             val_dataloader=val_dataloader,
-            test_dataloader=test_dataloader,
+            test_dataloader=test_loaders,
             n_iteration=n_iteration,
             checkpoint_path=f"{exp_path}/fold{fold}"
         );
@@ -125,24 +110,28 @@ def main():
         fold_stats[fold] = exp_results;
 
     # print results
-    metrics: List[str] = list(list(fold_stats[0]["statistics"].values())[0].keys())
-    names: List[str] = list(fold_stats[0]["statistics"].keys());
+    metrics: List[str] = list(list(fold_stats[0].values())[0]["statistics"].keys());
+    names: List[str] = list(fold_stats[0].keys());
     template: str = "fold\t\t"+"\t\t".join(metrics)+"\n";
 
-    avgs: Dict[str, Any] = {name: {metric: [] for metric in metrics} for name in names};
+    # compute average of all folds to get final results
+    avgs: Dict[str, Any] = {name: {metric: [] for metric in metrics} for name in names};  # initialize template
     for f, result in fold_stats.items(): # iterate over fold
-        for i, (name, name_data) in enumerate(result["statistics"].items()):  # iterate over test
-            line = "";
+        for i, (name, name_data) in enumerate(result.items()):  # iterate over test loader
+            # name: test loader name
+            # name_data: {}
+            line = "";  # init line
             if i == 0:
                 line += f"{f} - ({name})\t\t";
             else:
                 line += f"  - ({name})\t\t";
-            for metric, value in name_data.items():  # iterate over metric
+            for metric, value in name_data["statistics"].items():  # iterate over metric of each test loader
                 line += f"{value['mean']:.4f}±{value['std']:.4f}\t\t";
                 avgs[name][metric].append(value["mean"]);
             template += f"{line}\n"
     template += "\n"
 
+    # aggregate all fold results
     aggregated_result: Dict[str, float] = {
         test_name: {
             k: sum(v)/len(v) 
@@ -151,7 +140,7 @@ def main():
     };
     fold_stats["summary"] = aggregated_result;
 
-
+    # pretty print results
     for test_name, test_result in aggregated_result.items():
         template += f"\n**** {test_name} ****\n"
         line: str = "Avg";
@@ -166,9 +155,11 @@ def main():
     print("*"*20);
     print(template)
     
+    # save experiment results
     with open(f"{exp_path}/exp_results.json", "w") as f:
         json.dump(fold_stats, f, indent=4);
 
 
 if __name__ == "__main__":
-    main();
+    args = run_parser();
+    main(args);
